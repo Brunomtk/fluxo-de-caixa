b8702f3957e2a83791aeb044c423058b301699f1
Bruno Mendes da Silva <88750125+Brunomtk@users.noreply.github.com>
2023-06-12 16:10:02 -0300

    Add files via upload

---
+++ database/migrations/2021_03_09_191111_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->float('preco');
+            $table->string('categoria');
+            $table->integer('quantidade');
+            $table->date('datacompra')->default(null)->nullable();
+            $table->date('dataval')->default(null)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
---
+++ database/migrations/2021_05_18_153310_create_caixas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCaixasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('caixas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->float('preco');
+            $table->string('categoria');
+            $table->integer('quantidade');
+            $table->string('operacao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('caixas');
+    }
+}
